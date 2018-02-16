8067e9b927a3a9ec91f718c09d42724edb077ab4
Andrew Koltyakov <koltyakov@gmail.com>
2018-02-16 11:07:37 +0300

    Environment polyfill

---
+++ src/utils/env.ts
@@ -0,0 +1,40 @@
+export interface IPageContextInfo extends _spPageContextInfo {}
+
+export const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '[::1]' ||
+  window.location.hostname
+    .match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+export const loadPageContext = (): Promise<IPageContextInfo> => {
+  return new Promise(resolve => {
+    if (typeof _spPageContextInfo !== 'undefined') {
+      return resolve(_spPageContextInfo);
+    }
+    if (isLocalhost) {
+      const restUrl = `${window.location.origin}/_api/web?` +
+        `$select=Title,Language,ServerRelativeUrl,CurrentUser/Id,Url&` +
+        `$expand=CurrentUser/Id`;
+
+      return fetch(restUrl, {
+        method: 'GET',
+        headers: [['Accept', 'application/json;odata=verbose']]
+      })
+        .then(res => res.json())
+        .then(({ d: webInfo }) => {
+          const _spPageContextInfoFake: IPageContextInfo = {
+            ...{} as any,
+            webTitle: webInfo.Title,
+            webAbsoluteUrl: window.location.origin + webInfo.ServerRelativeUrl,
+            webServerRelativeUrl: webInfo.ServerRelativeUrl,
+            currentLanguage: webInfo.Language,
+            userId: webInfo.CurrentUser.Id,
+            __webAbsoluteUrl: webInfo.Url
+          };
+          (window as any)._spPageContextInfo = _spPageContextInfoFake;
+          return resolve(_spPageContextInfoFake);
+        });
+    }
+  });
+};

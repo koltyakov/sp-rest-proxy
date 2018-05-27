901d3bd9c01117c02f919a73918948c91b05fd23
Andrew Koltyakov <koltyakov@gmail.com>
2018-05-27 22:03:22 +0300

    Misc

--- test/manual/server.ts
+++ test/manual/server.ts
@@ -1,7 +1,7 @@
 import RestProxy from '../../src/RestProxy';
 
 (new RestProxy({
-  configPath: './config/integration/private.2013.json',
+  configPath: './config/private.json',
   staticRoot: './test/manual/static',
   protocol: 'http'
 })).serve();

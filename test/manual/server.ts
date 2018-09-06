c5159eca8cea3352717f17baa2cc6c5395edabfa
Andrew Koltyakov <koltyakov@gmail.com>
2018-09-06 10:53:57 +0300

    Fiddler support

--- test/manual/server.ts
+++ test/manual/server.ts
@@ -4,6 +4,10 @@ import RestProxy, { IProxySettings } from '../../src/RestProxy';
 
 const argv = parseArgs(process.argv.slice(2));
 
+process.env.http_proxy = 'http://127.0.0.1:8888';
+process.env.https_proxy = 'http://127.0.0.1:8888';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const settings: IProxySettings = {
   configPath: argv.p || './config/private.json',
   staticRoot: './test/manual/static',

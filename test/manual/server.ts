8901c60be6c2a92a7439cbcde89eca414fbf204d
Andrew Koltyakov <koltyakov@gmail.com>
2018-10-30 14:12:19 +0300

    Misc

--- test/manual/server.ts
+++ test/manual/server.ts
@@ -14,7 +14,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const settings: IProxySettings = {
   configPath: argv.p || './config/private.json',
   staticRoot: './test/manual/static',
-  logLevel: LogLevel.Verbose
+  logLevel: LogLevel.Info
   // protocol: 'https',
   // hostname: '10.42.7.50',
   // port: 3777

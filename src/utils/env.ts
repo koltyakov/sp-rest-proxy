2b3b85c42a269f627628a14d42215cfb29dd2e31
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-04-18 12:22:11 +0300

    Extend env helper with more context properties

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -14,28 +14,51 @@ export const loadPageContext = (): Promise<IPageContextInfo> => {
       return resolve(_spPageContextInfo);
     }
     if (isLocalhost) {
-      const restUrl = trimMultiline(`
-        ${window.location.origin}/_api/web?
-          $select=Title,Language,ServerRelativeUrl,CurrentUser/Id,Url&
-          $expand=CurrentUser/Id
-      `);
-      return fetch(restUrl, {
-        method: 'GET',
-        headers: [
-          ['Accept', 'application/json;odata=verbose'],
-          ['X-ProxyStrict', 'false']
-        ]
-      })
-        .then(res => res.json())
-        .then(({ d: webInfo }) => {
+      const getWebInfo = (): Promise<any> => {
+        const restUrl = trimMultiline(`
+          ${window.location.origin}/_api/web?
+            $select=Title,Language,ServerRelativeUrl,Url,
+              CurrentUser/Id,CurrentUser/LoginName,CurrentUser/Email,CurrentUser/Title&
+            $expand=CurrentUser
+        `);
+        return fetch(restUrl, {
+          method: 'GET',
+          headers: [
+            ['Accept', 'application/json;odata=verbose'],
+            ['X-ProxyStrict', 'false']
+          ]
+        }).then(res => res.json());
+      };
+      const getSiteInfo = (): Promise<any> => {
+        const restUrl = `${window.location.origin}/_api/site?$select=ServerRelativeUrl,Url`;
+        return fetch(restUrl, {
+          method: 'GET',
+          headers: [
+            ['Accept', 'application/json;odata=verbose'],
+            ['X-ProxyStrict', 'false']
+          ]
+        }).then(res => res.json());
+      };
+      return Promise.all([ getWebInfo(), getSiteInfo() ])
+        .then(([{ d: webInfo }, { d: siteInfo }]) => {
           const _spPageContextInfoFake: IPageContextInfo = {
             ...{} as any,
+            // Web info
             webTitle: webInfo.Title,
             webAbsoluteUrl: window.location.origin + webInfo.ServerRelativeUrl,
             webServerRelativeUrl: webInfo.ServerRelativeUrl,
             currentLanguage: webInfo.Language,
+            // Site info
+            siteAbsoluteUrl: window.location.origin + siteInfo.ServerRelativeUrl,
+            siteServerRelativeUrl: siteInfo.ServerRelativeUrl,
+            // User info
             userId: webInfo.CurrentUser.Id,
-            __webAbsoluteUrl: webInfo.Url
+            userLoginName: webInfo.CurrentUser.LoginName,
+            userDisplayName: webInfo.CurrentUser.Title,
+            userEmail: webInfo.CurrentUser.Email,
+            // Misc
+            __webAbsoluteUrl: webInfo.Url,
+            __siteAbsoluteUrl: siteInfo.Url,
           };
           (window as any)._spPageContextInfo = _spPageContextInfoFake;
           return resolve(_spPageContextInfoFake);

6c0002066132d39c8133ad3f96e58f827bcfe6fd
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-12-10 11:55:01 +0300

    spOdataEntity URI resolver, #90

--- src/core/routers/restGet.ts
+++ src/core/routers/restGet.ts
@@ -50,6 +50,10 @@ export class RestGetRouter extends BasicRouter {
         if (typeof r.body['odata.metadata'] === 'string') {
           r.body['odata.metadata'] = this.util.buildProxyEndpointUrl(r.body['odata.metadata']);
         }
+        // OData patch to PnPjs URI resolver
+        if (r.body.d && r.body.d.__metadata && r.body.d.__metadata.uri) {
+          r.body.d.__metadata.uri = this.util.buildProxyEndpointUrl(r.body.d.__metadata.uri);
+        }
         return r;
       })
       .then((r) => this.transmitResponse(res, r))

d39b00769a4d2a19e836a36c63239699a3a7d0d5
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-07-03 14:45:26 +0300

    Sample assets update

--- examples/simpleserver/server.js
+++ examples/simpleserver/server.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const RestProxy = require('sp-rest-proxy');
 
-(new RestProxy({
+new RestProxy({
   configPath: './config/private.json',
-  staticRoot: './static',
   rawBodyLimitSize: '4MB'
-})).serve();
+}).serve();
